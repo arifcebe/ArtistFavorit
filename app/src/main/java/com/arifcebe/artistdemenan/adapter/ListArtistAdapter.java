6eb59169bcce54efad75a6f6439adc01c3059cf3
arif cebe <arif.cahyab@gmail.com>
2016-01-16 23:32:16 +0700

    tambahkan gambar dan list artist

--- app/src/main/java/com/arifcebe/artistdemenan/adapter/ListArtistAdapter.java
+++ app/src/main/java/com/arifcebe/artistdemenan/adapter/ListArtistAdapter.java
@@ -1,33 +1,79 @@
 package com.arifcebe.artistdemenan.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.arifcebe.artistdemenan.R;
+import com.arifcebe.artistdemenan.model.Artist;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 /**
  * Created by arifcebe on 16/01/16.
  */
 public class ListArtistAdapter extends BaseAdapter {
 
+    private Context context;
+    private List<Artist> list;
+    private ListArtistHolder holder;
 
+    public ListArtistAdapter(Context context, List<Artist> list) {
+        this.context = context;
+        this.list = list;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return list.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(context)
+                    .inflate(R.layout.list_artist_item,null);
+            holder = new ListArtistHolder();
+            holder.img = (ImageView) convertView.findViewById(R.id.item_img);
+            holder.name = (TextView) convertView.findViewById(R.id.item_name);
+            holder.status = (TextView) convertView.findViewById(R.id.item_status);
+            holder.panggilan = (TextView) convertView.findViewById(R.id.item_panggilan);
+
+            convertView.setTag(holder);
+        }else{
+            holder = (ListArtistHolder) convertView.getTag();
+        }
+        Artist artist = list.get(position);
+        holder.name.setText(artist.getName());
+        holder.status.setText(artist.getHoby());
+        holder.panggilan.setText(artist.getPanggilan());
+
+        Glide.with(context)
+                .load(artist.getPhoto())
+                .into(holder.img);
+
+        return convertView;
+    }
+
+    private class ListArtistHolder {
+        ImageView img;
+        TextView name,status,panggilan;
     }
 }
+
--- app/src/main/java/com/arifcebe/artistdemenan/view/ListArtistActivity.java
+++ app/src/main/java/com/arifcebe/artistdemenan/view/ListArtistActivity.java
@@ -2,16 +2,95 @@ package com.arifcebe.artistdemenan.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.arifcebe.artistdemenan.R;
+import com.arifcebe.artistdemenan.adapter.ListArtistAdapter;
+import com.arifcebe.artistdemenan.config.BaseApplication;
+import com.arifcebe.artistdemenan.config.Webservice;
+import com.arifcebe.artistdemenan.model.Artist;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListArtistActivity extends AppCompatActivity {
 
+    private Button btnArtist;
+    private ListView lv;
 
+    private List<Artist> listArtist = new ArrayList<Artist>();
+    private ListArtistAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_artist_activity);
+
+        btnArtist = (Button) findViewById(R.id.artist_btnArtist);
+        lv= (ListView) findViewById(R.id.listview);
+        adapter = new ListArtistAdapter(this,listArtist);
+        lv.setAdapter(adapter);
+
+        btnArtist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestArtist();
+            }
+        });
+
+    }
+
+    private void requestArtist(){
+        BaseApplication.getInstance().startLoader(this);
+        JsonArrayRequest request = new JsonArrayRequest(Webservice.getArtist(),
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        BaseApplication.getInstance().stopLoader();
+                        Log.d("artist","response artist "+response.toString());
+                        generateListArtist(response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        BaseApplication.getInstance().stopLoader();
+                    }
+                });
+
+        BaseApplication.getInstance().addToRequestQueue(request, "artist");
+    }
+
+    /**
+     * generate list artist masukkan ke dalam model dan taruh ke adapter
+     * @param response
+     */
+    private void generateListArtist(JSONArray response){
+        for (int i = 0; i < response.length();i++){
+            try {
+                JSONObject jo = response.getJSONObject(i);
+                Artist artist = new Artist();
+                artist.setName(jo.getString("name"));
+                artist.setHoby(jo.getString("hoby"));
+                artist.setPanggilan(jo.getString("panggilan"));
+                artist.setPhoto(jo.getString("photo"));
+
+                listArtist.add(artist);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        adapter.notifyDataSetChanged();
     }
 }
